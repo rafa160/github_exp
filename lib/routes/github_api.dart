ca05f2590b4d8ccce5dc808178eb1755573b091a
Rafael Paz <rafaelpaz86@gmail.com>
2020-11-07 13:37:16 -0300

    Fix

--- lib/routes/github_api.dart
+++ lib/routes/github_api.dart
@@ -1,7 +1,7 @@
 class GitHubApi {
 
   static const api = 'https://api.github.com/';
-  static const token = 'd39fbf40b7a8a2cee1d79cc81ee470db64b5e7f3';
+  static const token = '';
 
   static const repositories = '${api}repositories';
   static const users = '${api}users/';
